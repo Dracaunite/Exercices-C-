788db8fff3a2697d927689f5de630c2a03079288
Administrateur <Administrateur@W10PRO>
2022-12-13 17:32:07 +0100

    Exercice POO Asterix pas terminé

---
+++ SlnAsterix/SlnAsterix/Personnage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlnAsterix
+{
+    class Personnage
+    {
+        // attributs (1 par ligne normalement)
+        private string nom, adresse, fonction, arme;
+        private int nbCombats, force, nbVictoire;
+        private bool potion;
+
+        private Personnage (string unNom, string uneAdresse, string uneFonction, int unNbCombat, string uneArme, bool unePotion, int uneForce, int unNbVictoire)
+        {
+            this.nom = unNom;
+            this.adresse = uneAdresse;
+            this.fonction = uneFonction;
+            this.nbCombats = unNbCombat;
+            this.force = uneForce;
+            this.nbVictoire = unNbVictoire;
+            this.potion = unePotion;
+        }
+    }
+}
